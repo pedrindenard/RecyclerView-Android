77c2265232bef9043cb2cd0a5a33069fc3f21502
pedrindenard <pedrominuzzi21@gmail.com>
2021-01-20 10:14:52 -0300

    Adicionando classes NotaDAO, Nota e ListaNovasAdapter

--- app/src/main/java/com/app/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/com/app/ceep/ui/activity/ListaNotasActivity.java
@@ -1,10 +1,17 @@
 package com.app.ceep.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ListView;
 
 import com.app.ceep.R;
+import com.app.ceep.dao.NotaDAO;
+import com.app.ceep.model.Nota;
+import com.app.ceep.ui.adapter.ListaNotasAdapter;
+
+import java.util.List;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -12,5 +19,14 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listaNotas = findViewById(R.id.listView);
+
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira nota", "Primeira descrição"));
+        List<Nota> todasNotas = dao.todos();
+
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
+
     }
 }
